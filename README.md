06c27b19cee4be2f0107a7dfee343f426a024e70
VersaliXis <versalix@protonmail.com>
2024-12-04 13:26:20 +0100

    added wallet save as pem

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # wallet-creator
+**Testnet only**
 Create wallets on a specific shard of MultiversX and request testnet EGLD in one only command
 
 
@@ -6,14 +7,19 @@ Create wallets on a specific shard of MultiversX and request testnet EGLD in one
 ## Installation
 ```
 git clone https://github.com/VersaliXis/wallet-creator.git
+python -m venv [/path to venv]
+source [/path to venv]/bin/activate
 pip install multiversx-sdk
 ```
 
 ## Use 
 ```
+source [/path to venv]/bin/activate
 python main.py
 ```
 
 ## Explanation
-The script `main.py` creates 3 wallets on each of the three shards.
-It then calls the API of the faucet `r3d4.fr` to get 1 EGLD on Testnet
+The script `main.py` creates 3 wallets on each of the three shards.  
+It saves pem in `./wallets/`  
+It then calls the API of the faucet `r3d4.fr` to get 1 EGLD on Testnet  
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
-from multiversx_sdk import Mnemonic, Address
+from pathlib import Path
+from multiversx_sdk import Mnemonic, Address, UserPEM
 from multiversx_sdk.core.address import get_shard_of_pubkey
 from use_faucet import request_testnet_EGLD
 
+PATH = Path("./wallets")
 NUMBER_OF_SHARDS = 3
 AVAILABLE_SHARDS = [0,1,2]
 MAX_ITERATIONS = 100
@@ -14,20 +15,23 @@ class ExceededMaxLoopIterationException(Exception):
 #Generate 3 wallets in each of the shards
 def generate_and_fill_wallets():
     for shard_id in AVAILABLE_SHARDS:
-        for _ in range(3):
-            address, mnemonic = new_mnemonic_in_shard(shard_id)
+        for i in range(3):
+            address, mnemonic = new_mnemonic_in_shard(i, shard_id)
             print(f"\n\nGenerated {address} in shard {shard_id}")
             request_testnet_EGLD(address)
 
 #Create a new mnemonic in a given shard
-def new_mnemonic_in_shard(shard_index: int) -> (str, Mnemonic):
+def new_mnemonic_in_shard(wallet_id: int, shard_index: int) -> (str, Mnemonic):
     for _ in range(MAX_ITERATIONS):
         mnemonic = Mnemonic.generate()
+        secret_key = mnemonic.derive_key(0)
         pubkey = mnemonic.derive_key().generate_public_key()
         generated_address_shard = get_shard_of_pubkey(pubkey.buffer, NUMBER_OF_SHARDS)
 
         if shard_index == generated_address_shard:
             address = Address(pubkey.buffer, "erd").to_bech32()
+            pem = UserPEM(label = address, secret_key=secret_key)
+            pem.save(PATH / f"wallet_{wallet_id}_shard_{shard_index}.pem")
             return address, mnemonic
 
     raise ExceededMaxLoopIterationException
